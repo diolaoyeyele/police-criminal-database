920e01f4ea754cb67ecbb403a879780756fdd472
diolaoyeyele <didi.oyeyele@gmail.com>
2019-05-03 16:05:10 +0100

    firstcommit

---
+++ login.php
@@ -0,0 +1,19 @@
+
+<?php /*echo $_POST["id"];
+Item Bought: <?php echo $_POST["id"];*/
+
+ <?php 
+    $var = $_POST["id"];
+    $var2 = $_POST["password"]
+    if( ($var == "diola" && $var2 == "minato")
+    {
+     
+	}
+	
+	
+	else{
+	    echo "price was not specified"
+	}
+	?>
+	</body>
+	</html>
